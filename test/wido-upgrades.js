c1ece87267451f70b848554d74951a4d584ebcb6
Kunal Jain <jainkunal3004@gmail.com>
2021-12-21 18:12:13 +0530

    fix: flaky upgrade test

--- test/wido-upgrades.js
+++ test/wido-upgrades.js
@@ -7,10 +7,10 @@ describe("WidoProxy", function () {
     it('works before and after upgrading', async function () {
         const Wido = await ethers.getContractFactory("Wido");
         const instance = await upgrades.deployProxy(Wido, [42]);
-        expect(await instance.DOMAIN_SEPARATOR()).to.equal("0x353e21220e1f764f03cb80657db16c5fdfba8d9326aafbe4c4233374b93d7ecf");
+        const domainSeparator = await instance.DOMAIN_SEPARATOR();
 
         const WidoV2 = await ethers.getContractFactory("Wido");
         await upgrades.upgradeProxy(instance.address, WidoV2);
-        expect(await instance.DOMAIN_SEPARATOR()).to.equal("0x353e21220e1f764f03cb80657db16c5fdfba8d9326aafbe4c4233374b93d7ecf");
+        expect(await instance.DOMAIN_SEPARATOR()).to.equal(domainSeparator);
     });
 });
