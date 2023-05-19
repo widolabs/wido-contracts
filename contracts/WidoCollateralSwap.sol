bad7196b28e55d70f620f0c6c62835f56a2b8069
sandro <devspuig@pm.me>
2023-05-19 07:23:37 +0200

    fixes & docs

--- contracts/WidoCollateralSwap.sol
+++ contracts/WidoCollateralSwap.sol
@@ -4,15 +4,18 @@ pragma solidity 0.8.7;
 
 import {IERC3156FlashBorrower, IERC3156FlashLender} from "./interfaces/IERC3156.sol";
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import {IWidoRouter} from "./interfaces/IWidoRouter.sol";
 import {IWidoTokenManager} from "./interfaces/IWidoTokenManager.sol";
 import {IComet} from "./interfaces/IComet.sol";
-import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract WidoCollateralSwap is IERC3156FlashBorrower {
     using SafeMath for uint256;
 
-    IERC3156FlashLender public flashLoanProvider;
+    /// @dev The used flash loan provider
+    IERC3156FlashLender immutable flashLoanProvider;
+
+    /// @dev The typehash for the ERC-3156 `onFlashLoan` return
     bytes32 internal constant ON_FLASH_LOAN_RESPONSE = keccak256("ERC3156FlashBorrower.onFlashLoan");
 
     struct Collateral {
@@ -41,6 +44,12 @@ contract WidoCollateralSwap is IERC3156FlashBorrower {
         flashLoanProvider = _flashLoanProvider;
     }
 
+    /// @notice Performs a collateral swap
+    /// @param existingCollateral The collateral currently locked in the Comet contract
+    /// @param finalCollateral The final collateral desired collateral
+    /// @param sigs The required signatures to allow and revoke permission to this contract
+    /// @param swap The necessary data to swap one collateral for the other
+    /// @param comet The address of the Comet contract to interact with
     function swapCollateral(
         Collateral calldata existingCollateral,
         Collateral calldata finalCollateral,
@@ -64,6 +73,8 @@ contract WidoCollateralSwap is IERC3156FlashBorrower {
         );
     }
 
+    /// @notice Callback to be executed by the flash loan provider
+    /// @dev Only allow-listed providers should have access
     function onFlashLoan(
         address /* initiator */,
         address borrowedAsset,
@@ -71,13 +82,15 @@ contract WidoCollateralSwap is IERC3156FlashBorrower {
         uint256 fee,
         bytes calldata data
     ) external override returns (bytes32) {
-        require(msg.sender == address(flashLoanProvider), "Caller is not Euler");
+        require(msg.sender == address(flashLoanProvider), "Caller is not accepted provider");
+        require(fee == 0, "Fee payment not supported yet");
+
         (
         address user,
         IComet comet,
         Collateral memory existingCollateral,
         Signatures memory signatures,
-        WidoSwap memory swap
+        WidoSwap memory swapDetails
         ) = abi.decode(
             data,
             (address, IComet, Collateral, Signatures, WidoSwap)
@@ -89,30 +102,11 @@ contract WidoCollateralSwap is IERC3156FlashBorrower {
         // withdraw existing collateral
         _withdrawFrom(comet, user, existingCollateral, signatures);
 
-        // store amount of final collateral before swap
-        uint256 surplusAmount = IERC20(borrowedAsset).balanceOf(address(this));
+        // execute swap
+        _swap(existingCollateral, swapDetails);
 
-        {
-            // approve WidoTokenManager initial collateral to make the swap
-            IERC20(existingCollateral.addr).approve(
-                swap.tokenManager,
-                existingCollateral.amount
-            );
-
-            // execute swap
-            (bool success, bytes memory result) = swap.router.call(swap.callData);
-
-            if (!success) {
-                if (result.length < 68) revert("WidoRouter failed");
-                assembly {
-                    result := add(result, 0x04)
-                }
-                revert(abi.decode(result, (string)));
-            }
-        }
-
-        // check amount of final collateral after swap
-        surplusAmount = IERC20(borrowedAsset).balanceOf(address(this)) - borrowedAmount;
+        // check amount of surplus collateral
+        uint256 surplusAmount = IERC20(borrowedAsset).balanceOf(address(this)) - borrowedAmount;
 
         // if positive slippage, supply extra to user
         if (surplusAmount > 0) {
@@ -126,6 +120,29 @@ contract WidoCollateralSwap is IERC3156FlashBorrower {
         );
 
         return ON_FLASH_LOAN_RESPONSE;
+    }
+
+    /// @dev Performs the swap of the collateral on the WidoRouter
+    function _swap(
+        Collateral memory collateral,
+        WidoSwap memory swap
+    ) internal {
+        // approve WidoTokenManager initial collateral to make the swap
+        IERC20(collateral.addr).approve(
+            swap.tokenManager,
+            collateral.amount
+        );
+
+        // execute swap
+        (bool success, bytes memory result) = swap.router.call(swap.callData);
+
+        if (!success) {
+            if (result.length < 68) revert("WidoRouter failed");
+            assembly {
+                result := add(result, 0x04)
+            }
+            revert(abi.decode(result, (string)));
+        }
     }
 
     /// @dev Supplies collateral on behalf of user
@@ -155,7 +172,7 @@ contract WidoCollateralSwap is IERC3156FlashBorrower {
         // withdraw assets
         comet.withdrawFrom(user, address(this), collateral.addr, collateral.amount);
         // increment nonce
-        unchecked {nonce++;}
+        unchecked { nonce++; }
         // revoke permission
         _allowBySig(comet, user, false, nonce, sigs.revoke);
     }
