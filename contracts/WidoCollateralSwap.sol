f59a1dc116750546b54763d242671290ab976e55
sandro <devspuig@pm.me>
2023-05-26 15:22:57 +0200

    Follow common natspec

--- contracts/WidoCollateralSwap.sol
+++ contracts/WidoCollateralSwap.sol
@@ -123,16 +123,14 @@ contract WidoCollateralSwap is IERC3156FlashBorrower, IFlashLoanSimpleReceiver {
         );
     }
 
-    /**
-    * @notice Executes an operation after receiving the flash-borrowed asset
-    * @dev Ensure that the contract can return the debt + premium, e.g., has
-    *      enough funds to repay and has approved the Pool to pull the total amount
-    * @param asset The address of the flash-borrowed asset
-    * @param amount The amount of the flash-borrowed asset
-    * @param premium The fee of the flash-borrowed asset
-    * @param params The byte-encoded params passed when initiating the flashloan
-    * @return True if the execution of the operation succeeds, false otherwise
-    */
+    /// @notice Executes an operation after receiving the flash-borrowed asset
+    /// @dev Ensure that the contract can return the debt + premium, e.g., has
+    ///      enough funds to repay and has approved the Pool to pull the total amount
+    /// @param asset The address of the flash-borrowed asset
+    /// @param amount The amount of the flash-borrowed asset
+    /// @param premium The fee of the flash-borrowed asset
+    /// @param params The byte-encoded params passed when initiating the flashloan
+    /// @return True if the execution of the operation succeeds, false otherwise
     function executeOperation(
         address asset,
         uint256 amount,
