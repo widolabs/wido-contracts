1fd7de725459b1c7acdd9f3f41a0e19cebb8353f
sandro <devspuig@pm.me>
2023-08-04 12:06:19 +0700

    add script to deploy UniV3

---
+++ script/core/zapper/WidoZapperUniswapV3.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.7;
+
+import "forge-std/Script.sol";
+import "../../../contracts/core/zapper/WidoZapperUniswapV3.sol";
+
+contract WidoZapperUniswapV3Script is Script {
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerKey);
+
+        new WidoZapperUniswapV3();
+
+        vm.stopBroadcast();
+    }
+}
