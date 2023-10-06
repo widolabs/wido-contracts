67313e98a2a3366715bbad2aa76cc4883277e72a
Roman <barabanov1233@gmail.com>
2023-10-06 11:40:30 +0400

    Use revert instead of require

--- contracts/compound/libraries/LibCollateralSwap.sol
+++ contracts/compound/libraries/LibCollateralSwap.sol
@@ -10,6 +10,7 @@ library LibCollateralSwap {
     using SafeMath for uint256;
 
     error WidoRouterFailed();
+    error InvalidSwapDestinationToken();
 
     struct Collateral {
         address addr;
@@ -66,7 +67,7 @@ library LibCollateralSwap {
 
         // Ensure that the swap operation has returned the correct token by comparing the balances before and after the swap.
         // This step verifies that the swap destination token type is the same as the flash loan token type, to avoid accidentally swapping to an incorrect asset.
-        require(IERC20(borrowedAsset).balanceOf(address(this)) > borrowedAssetBalanceBeforeSwap, "Destination token should be the same as the flash loan token");
+        if (IERC20(borrowedAsset).balanceOf(address(this)) <= borrowedAssetBalanceBeforeSwap) revert InvalidSwapDestinationToken();
 
         // check amount of surplus collateral
         uint256 surplusAmount = IERC20(borrowedAsset).balanceOf(address(this)) - borrowedAmount - fee;
