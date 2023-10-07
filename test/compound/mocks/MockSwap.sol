84b8e5a9a17a7e2a8a7b5496990f10db00ff4673
Roman <30427286+barabanovro@users.noreply.github.com>
2023-10-07 09:53:41 +0400

    Add test: invalid token swap scenario (#39)

--- test/compound/mocks/MockSwap.sol
+++ test/compound/mocks/MockSwap.sol
@@ -7,14 +7,21 @@ contract MockSwap {
     using SafeTransferLib for ERC20;
     ERC20 immutable weth;
     ERC20 immutable wbtc;
+    ERC20 immutable usdc;
 
-    constructor(ERC20 _weth, ERC20 _wbtc) {
+    constructor(ERC20 _weth, ERC20 _wbtc, ERC20 _usdc) {
         weth = _weth;
         wbtc = _wbtc;
+        usdc = _usdc;
     }
 
     function swapWbtcToWeth(uint256 wbtcAmount, uint256 wethAmount, address recipient) external {
         wbtc.safeTransferFrom(msg.sender, address(this), wbtcAmount);
         weth.safeTransfer(recipient, wethAmount);
+    }
+
+    function swapWbtcToUsdc(uint256 wbtcAmount, uint256 usdcAmount, address recipient) external {
+        wbtc.safeTransferFrom(msg.sender, address(this), wbtcAmount);
+        usdc.safeTransfer(recipient, usdcAmount);
     }
 }
